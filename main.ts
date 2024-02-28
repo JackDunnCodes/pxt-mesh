2b85517f071a9273c15d4da21391e57f4085914c
Jack Dunn <56315967+JackDunnCodes@users.noreply.github.com>
2024-02-28 22:00:42 +0000

    yet more debug

--- main.ts
+++ main.ts
@@ -97,12 +97,14 @@ namespace mesh {
             const offset = 7;
             if(!this.cache) 
                 for (let i = this.sliceIndices[0]; i < this.sliceIndices[1]; i++){
+                    serial.writeLine(">> "+this.cache);
                     this.cache += String.fromCharCode(this.data[i])
                 }
             return this.cache;
         }
 
         set stringPayload(val: string) {
+            this.cache = null;
             const offset = 7;
             if (offset) {
                 const buf = control.createBufferFromUTF8(truncateString(val, 248));
